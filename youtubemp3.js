c6b63a9bde206f8c354e5f86206e526c6feee289
GuticKenan <gutickenan@hotmail.com>
2020-06-29 23:20:57 +0200

    User path

--- youtubemp3.js
+++ youtubemp3.js
@@ -1,18 +1,18 @@
 var YoutubeMp3Downloader = require('youtube-mp3-downloader');
 const pathToFfmpeg = require('ffmpeg-static');
-
-const _path = process.env.USERPROFILE + '\\' + 'Downloads' + '\\';
+const downloadsFolder = require('downloads-folder');
+//const _path = process.env.USERPROFILE + '\\' + 'Downloads' + '\\';
 
 var YD = new YoutubeMp3Downloader({
   ffmpegPath: pathToFfmpeg,
-  outputPath: _path,
+  outputPath: downloadsFolder(),
   youtubeVideoQuality: 'highest',
   queueParallelism: 2,
   progressTimeout: 2000,
 });
 
 function YTDownload(videoId, title) {
-	console.log(_path);
+	
   YD.download(
     videoId,
     title.replace('|', '').replace(/[^a-zA-Z0-9]/g,'') + Math.random().toString()
